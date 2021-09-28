1333505fbbbde9d4bc186f74087eaea0daa7f445
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-09-28 18:49:17 -0400

    Create start.py

---
+++ start.py
@@ -0,0 +1,17 @@
+import os
+try:
+    import requests
+except ModuleNotFoundError:
+    os.system('pip install requests')
+    os.system('clear')
+try:
+    import time
+except ModuleNotFoundError:
+    os.system('pip install time')
+    os.system('clear')
+try:
+    import socket
+except ModuleNotFoundError:
+    os.system('pip install socket')
+    os.system('clear')
+os.system('bash r00t.sh')
